1dbb90f751019e3786534bc14337c7a8144cb7b4
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2025-03-21 16:28:13 +0600

    Update analytics-provider.tsx

--- src/components/analytics-provider.tsx
+++ src/components/analytics-provider.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import type React from "react"
-
 import { usePathname, useSearchParams } from "next/navigation"
 import { useEffect } from "react"
 import { trackPageView } from "@/lib/analytics"
